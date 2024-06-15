f1f6817e87f2a6a2499213fe294f44887f9a77e8
Vishesh <visheshvishu1@gmail.com>
2024-06-16 02:11:26 +0530

    ReadMe Updated

--- Model4/EfficientNet.py
+++ Model4/EfficientNet.py
@@ -9,8 +9,8 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 
-# Step 1: Define the path to your dataset
-path = Path('/Users/visheshgoyal/Python Projects/Leaf Project/MangoLeafDBAryaShah')
+# # Step 1: Define the path to your dataset
+# path = Path('/Users/visheshgoyal/Python Projects/Leaf Project/MangoLeafDBAryaShah')
 
 # Check if the path exists
 # if not path.exists():
@@ -18,20 +18,20 @@ path = Path('/Users/visheshgoyal/Python Projects/Leaf Project/MangoLeafDBAryaSha
 # else:
 #     print(f"Dataset path: {path}")
 
-# Step 2: Create the DataLoaders with transformations
-dls = ImageDataLoaders.from_folder(
-    path, 
-    valid_pct=0.2,  # Use 20% of data for validation
-    item_tfms=Resize(448),  # Resize images to 448x448
-    batch_tfms=aug_transforms(size=224, max_warp=0)  # Data augmentations
-)
+# # Step 2: Create the DataLoaders with transformations
+# dls = ImageDataLoaders.from_folder(
+#     path, 
+#     valid_pct=0.2,  # Use 20% of data for validation
+#     item_tfms=Resize(448),  # Resize images to 448x448
+#     batch_tfms=aug_transforms(size=224, max_warp=0)  # Data augmentations
+# )
 
 # # Step 3: Show length of training and testing data to verify
 # print(f'{len(dls.train_ds)}')
 # print(f'{len(dls.valid_ds)}')
 
-# Step 4: Create the learner with EfficientNet architecture
-learn = cnn_learner(dls, efficientnet_b0, metrics=[error_rate, accuracy, Precision()])
+# # Step 4: Create the learner with EfficientNet architecture
+# learn = cnn_learner(dls, efficientnet_b0, metrics=[error_rate, accuracy, Precision()])
 
 # # Step 5: Train the model
 # learn.fine_tune(5)  # Fine-tune for 5 epochs
@@ -41,36 +41,36 @@ learn = cnn_learner(dls, efficientnet_b0, metrics=[error_rate, accuracy, Precisi
 # learn.export('/Users/visheshgoyal/Python Projects/Leaf Project/Mangoleaf/Model4/Trained Model/EfficentNetFinal.pkl')
 
 # Step 7: Import the model
-learn = load_learner('//Users/visheshgoyal/Python Projects/Leaf Project/Mangoleaf/Model4/Trained Model/EfficentNetFinal.pkl')
+learn = load_learner('/Users/visheshgoyal/Python Projects/Leaf Project/Mangoleaf/Model4/Trained Model/EfficentNetFinal.pkl')
 
-# # Step 8: Load a new image and predict its class
-# path = '/Users/visheshgoyal/Downloads/Unknown.jpeg'
-# img = PILImage.create(path)
-# pred_class, pred_idx, probs = learn.predict(img)
-# probs *= 100
-# print(f'Prediction: {pred_class}, Probability: {probs[pred_idx]:.4f}%')
+# Step 8: Load a new image and predict its class
+path = '/Users/visheshgoyal/Desktop/Unknown.jpeg'
+img = PILImage.create(path)
+pred_class, pred_idx, probs = learn.predict(img)
+probs *= 100
+print(f'Prediction: {pred_class}, Probability: {probs[pred_idx]:.4f}%')
 
-# Step 9: Calculating metrics and confusion matrix
-preds, targs = learn.get_preds(dl=dls.valid)
-pred_labels = preds.argmax(dim=1)
-pred_labels = pred_labels.numpy()
-targs = targs.numpy()
-f1 = f1_score(targs, pred_labels, average='macro')
-acc = accuracy_score(targs, pred_labels)
-precision = precision_score(targs, pred_labels, average='macro')
-print(f"F1 Score: {f1}")
-print(f'Validation Accuracy: {acc:.4f}')
-print(f'Precision Score: {precision:.4f}')
+# # Step 9: Calculating metrics and confusion matrix
+# preds, targs = learn.get_preds(dl=dls.valid)
+# pred_labels = preds.argmax(dim=1)
+# pred_labels = pred_labels.numpy()
+# targs = targs.numpy()
+# f1 = f1_score(targs, pred_labels, average='macro')
+# acc = accuracy_score(targs, pred_labels)
+# precision = precision_score(targs, pred_labels, average='macro')
+# print(f"F1 Score: {f1}")
+# print(f'Validation Accuracy: {acc:.4f}')
+# print(f'Precision Score: {precision:.4f}')
 
-# Confusion Matrix
-class_labels = ["Healthy","Infected"] 
-cm = confusion_matrix(targs, pred_labels, labels=np.arange(len(class_labels)))
+# # Confusion Matrix
+# class_labels = ["Healthy","Infected"] 
+# cm = confusion_matrix(targs, pred_labels, labels=np.arange(len(class_labels)))
 
-# Convert confusion matrix to a DataFrame for visualization
-cm_df = pd.DataFrame(cm, index=class_labels, columns=class_labels)
-plt.figure(figsize=(10, 7))
-sns.heatmap(cm_df, annot=True, cmap='Blues', fmt='g')
-plt.title('Confusion Matrix')
-plt.xlabel('Predicted')
-plt.ylabel('Actual')
-plt.show()
+# # Convert confusion matrix to a DataFrame for visualization
+# cm_df = pd.DataFrame(cm, index=class_labels, columns=class_labels)
+# plt.figure(figsize=(10, 7))
+# sns.heatmap(cm_df, annot=True, cmap='Blues', fmt='g')
+# plt.title('Confusion Matrix')
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Mangoleaf
+Models required:
+    - Fast.AI
+    - Seaborn
+    - Math Plot Lib
+
+Brief:
+    - Hey guys, this repository has different image detection models. 
+    - The models are trained on a database of 8000 images of both healthy and infected mango plant leaves
+    - When you add a new mango leaf image, the models are capable of detecting the disease with about 98% accuracy(Different for each model)
+    - Fast.AI framework has been used quite extensively. 
+    - Models are: 
+        - The Resnet18 Model is the least effective one with accuracy of just 84%. 
+        - GoogleNet model has highest 99% accuracy but takes very long time to get trained on the dataset, about 6-7 hours per epoch
+        - EfficientNet model has 98% accuracy and takes just 57 minutes for one epoch on our database. 
+    - Python has been used. 
+
+The models in Trained Model folder with .pkl extension are ready to use models. 
+
+The commented lines are to be used as and when required for calculating metrics, adding new images, training the models etc but every line is important and has its role mentioned in the previous line. 
